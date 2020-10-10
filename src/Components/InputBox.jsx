91d9946434dc31bd8ad4ef53cec5044a8f1c709a
Sri Vishnu <ssrivishnu21@gmail.com>
2020-10-10 17:48:07 +0530

    added stylings

--- src/Components/InputBox.jsx
+++ src/Components/InputBox.jsx
@@ -3,10 +3,11 @@ import React, {FunctionComponent} from "react";
 export const InputBox: FunctionComponent = (props) => {
     return (
         <div>
-            <input type="number"
-                   placeholder={props.placeHolder}
-                   id={props.id}
-                   onChange={props.updateValue}
+            <input
+                type="number"
+                placeholder={props.placeHolder}
+                id={props.id}
+                onChange={props.updateValue}
             />
         </div>
     );
